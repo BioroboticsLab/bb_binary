2d3bee490c0a13b5e336000f3d38645132c20aaa
berleon <github@leon-sixt.de>
2016-06-02 13:41:41 +0200

    Add directory index functions

--- python/bb_binary/__init__.py
+++ python/bb_binary/__init__.py
@@ -2,8 +2,12 @@
 import capnp
 import os
 import numpy as np
+import json
+import math
+
 
 capnp.remove_import_hook()
+
 
 _dirname = os.path.dirname(os.path.realpath(__file__))
 bbb = capnp.load(os.path.join(_dirname, 'bb_binary_schema.capnp'))
@@ -84,26 +88,33 @@ class Repository:
     The Repository class manages multiple bb_binary files. It creates a
     directory layout that enables fast access by the timestamp.
     """
-    def __init__(self, root_dir, ts_start, ts_end, max_nb_files_in_dir=100):
+    def __init__(self, root_dir, ts_begin=0, directory_depths=None):
         """
         Opens the repository at `root_dir`
         """
         self.root_dir = root_dir
-        self.ts_start = ts_start
-        self.ts_end = ts_end
-        self.max_nb_files_in_dir = max_nb_files_in_dir
+        self.ts_begin = ts_begin
+        if directory_depths is None:
+            directory_depths = [3, 3, 3, 5]
+        self.directory_depths = directory_depths
         if not os.path.exists(self.repo_json_fname()):
             self._save_json()
 
     def repo_json_fname(self):
         return os.path.join(self.root_dir, 'bbb_repo.json')
 
+    @property
+    def max_ts(self):
+        return 10**sum(self.directory_depths) - 1
+
+    def can_contain_ts(self, timestamp):
+        return self.ts_begin <= timestamp < self.max_ts
+
     def to_config(self):
         return {
             'root_dir': self.root_dir,
-            'ts_start': self.ts_start,
-            'ts_end': self.ts_end,
-            'max_nb_files_in_dir': self.max_nb_files_in_dir,
+            'ts_begin': self.ts_begin,
+            'directory_depths': self.directory_depths,
         }
 
     def _save_json(self):
@@ -116,18 +127,133 @@ class Repository:
         """
         pass
 
-    def load(timestamp, cam) -> bbb.FrameContainer:
+    def _create_file_and_symlinks(self, begin_ts, end_ts, cam_ids,
+                                  extension=''):
+        fname = self.get_file_name(begin_ts, end_ts, cam_ids, extension)
+        os.makedirs(os.path.dirname(fname), exist_ok=True)
+
+        if not os.path.exists(fname):
+            open(fname, 'a').close()
+
+        iter_ts = end_ts
+        symlinks = []
+        while self.spans_multiple_directories(begin_ts, iter_ts):
+            dir_slices = self.directory_slices_for_ts(iter_ts)
+            link_fname = self.get_file_name(begin_ts, iter_ts, cam_ids,
+                                            extension, dir_slices)
+            symlinks.append(link_fname)
+            link_dir = os.path.dirname(link_fname)
+            os.makedirs(link_dir, exist_ok=True)
+            rel_goal = os.path.relpath(fname, start=link_dir)
+            os.symlink(rel_goal, link_fname)
+
+            iter_dir_slices = self.one_directory_earlier(iter_ts)
+            iter_ts = self.get_timestamp_for_directory_slice(iter_dir_slices)
+
+        return symlinks
+
+    def spans_multiple_directories(self, first_ts, end_ts):
+        return self.directory_slices_for_ts(first_ts) != \
+            self.directory_slices_for_ts(end_ts)
+
+    def open(timestamp, cam) -> bbb.FrameContainer:
         """
         Finds and load the FrameContainer that matches the timestamp and the
         cam.
         """
         pass
 
-    def find(timestamp) -> 'list(str)':
+    def get_timestamp_for_directory_slice(self, dir_slices):
+        dir_slices = list(map(int, dir_slices))
+        ts = 0
+        for d, dir_number in zip(self._cumsum_directory_depths(), dir_slices):
+            ts += dir_number * 10 ** d
+        return ts
+
+    def one_directory_earlier(self, dir_slices):
+        if type(dir_slices) == int:
+            dir_slices = self.directory_slices_for_ts(dir_slices)
+        ts = self.get_timestamp_for_directory_slice(dir_slices)
+        ts -= 10**self._cumsum_directory_depths()[-1]
+        return self.directory_slices_for_ts(ts)
+
+    def find(self, ts) -> 'list(str)':
         """
-        Returns all files that includes detections to the timestamp
+        Returns all files that includes detections to the given timestamp `ts`.
         """
-        pass
+        dir_slices = self.directory_slices_for_ts(ts)
+        try:
+            fnames = self.all_files_in(*dir_slices)
+        except FileNotFoundError:
+            return []
+
+        parts = [f.split('_') for f in fnames]
+        begin_end_fnames = [(int(p[0]), int(p[1]), f)
+                            for p, f in zip(parts, fnames)]
+
+        found_files = []
+        for begin, end, fname in begin_end_fnames:
+            if begin <= ts < end:
+                full_slices = dir_slices + [fname]
+                found_files.append(self.join_with_repo_dir(*full_slices))
+        return found_files
+
+    def get_file_name(self, begin_ts, end_ts, cam_ids, extension='',
+                      dir_slices=None):
+        if dir_slices is None:
+            dir_slices = self.directory_slices_for_ts(begin_ts)
+        cam_id_as_str = list(map(str, sorted(cam_ids)))
+        basename = "{begin}_{end}_cam_{cam_ids}".format(
+            cam_ids="".join(cam_id_as_str),
+            begin=begin_ts,
+            end=end_ts
+        )
+        if extension != '':
+            basename += '.' + extension
+        full_slices = dir_slices + [basename]
+        return self.join_with_repo_dir(*full_slices)
+
+    def all_files_in(self, *paths):
+        dirname = self.join_with_repo_dir(*paths)
+        return [f for f in os.listdir(dirname)
+                if os.path.isfile(os.path.join(dirname, f))]
+
+    def join_with_repo_dir(self, *paths):
+        return os.path.join(self.root_dir, *paths)
+
+    def _cumsum_directory_depths(self):
+        return [sum(self.directory_depths[i:-1])
+                for i in range(len(self.directory_depths))]
+
+    def directory_slices_for_ts(self, timestamp):
+        assert self.can_contain_ts(timestamp)
+
+        def convert_timestamp_to_path(ts, max_digets):
+            format_str = "{{:0{}d}}".format(max_digets)
+            return format_str.format(ts)
+
+        def split_number(n):
+            d = 0
+            depths = [sum(self.directory_depths[i:-1])
+                      for i in range(len(self.directory_depths))]
+            for d in depths:
+                n_d = int(math.floor(n / 10 ** d))
+                yield n_d
+                n -= n_d * 10 ** (d)
+
+        slices = []
+        for t, d in zip(split_number(timestamp), self.directory_depths):
+            slices.append(convert_timestamp_to_path(t, d))
+        return slices[:-1]
+
+    def common_directory_slices(self, first_ts, second_ts):
+        slices = []
+        for f, s in zip(self.directory_slices_for_ts(first_ts),
+                        self.directory_slices_for_ts(second_ts)):
+            if f == s:
+                slices.append(f)
+            else:
+                return slices
 
     @staticmethod
     def load(root_dir):
--- python/test/test_bb_binary.py
+++ python/test/test_bb_binary.py
@@ -2,7 +2,8 @@
 import bb_binary as bbb
 import time
 import numpy as np
-
+import pytest
+import os
 
 def test_bbb_is_loaded():
     frame = bbb.Frame.new_message()
@@ -65,8 +66,131 @@ def test_bbb_convert_detections_to_numpy():
 
 
 def test_bbb_repo_save_json(tmpdir):
-    repo = bbb.Repository(str(tmpdir), 0, 10000)
+    repo = bbb.Repository(str(tmpdir), 0)
     assert tmpdir.join('bbb_repo.json').exists()
 
     loaded_repo = bbb.Repository.load(str(tmpdir))
     assert repo == loaded_repo
+
+
+
+def test_bbb_repo_directory_slices_for_ts(tmpdir):
+    repo = bbb.Repository(str(tmpdir), ts_begin=0, directory_depths=[2]*4,
+                          )
+    dirs = list(repo.directory_slices_for_ts(3000))
+    assert dirs == ['00', '00', '30']
+
+    repo = bbb.Repository(str(tmpdir), ts_begin=1101, directory_depths=[3]*4)
+
+    dirs = list(repo.directory_slices_for_ts(58000))
+    assert dirs == ['000', '000', '058']
+
+    dirs = list(repo.directory_slices_for_ts(14358000))
+    assert dirs == ['000', '014', '358']
+
+    now = int(time.time())
+    dirs = list(repo.directory_slices_for_ts(now))
+    now = str(now)
+    assert dirs[-1] == now[-6:-3]
+    assert dirs[-2] == now[-9:-6]
+
+    dirs = list(repo.directory_slices_for_ts(repo.max_ts - 1))
+    assert dirs == ['999', '999', '999']
+
+    with pytest.raises(AssertionError):
+        repo.directory_slices_for_ts(repo.max_ts)
+
+
+def test_bbb_repo_get_ts_for_directory_slices(tmpdir):
+    repo = bbb.Repository(str(tmpdir), ts_begin=0, directory_depths=[2]*4)
+
+    dir_slices = ['00', '10', '01']
+    assert repo.get_timestamp_for_directory_slice(dir_slices) == 100100
+
+    # test inverse to directory_slices_for_ts
+    ts = 3000
+    dir_slices = list(repo.directory_slices_for_ts(ts))
+    assert repo.get_timestamp_for_directory_slice(dir_slices)
+
+
+def fill_repository(repo, begin_end_cam_ids):
+    for begin, end, cam_ids in begin_end_cam_ids:
+        params = begin, end, cam_ids, 'bbb'
+        fname = repo.get_file_name(*params)
+        os.makedirs(os.path.dirname(fname), exist_ok=True)
+        with open(fname, 'w+') as f:
+            f.write(str(begin))
+        repo._create_file_and_symlinks(*params)
+
+
+def find_and_assert_begin(repo, timestamp, expect_begin, nb_files_found=1):
+    fnames = repo.find(timestamp)
+    if type(expect_begin) == int:
+        expect_begin = [expect_begin]
+    assert type(expect_begin) in (tuple, list)
+    expect_begin = list(map(str, expect_begin))
+
+    assert len(fnames) == nb_files_found
+    for fname in fnames:
+        with open(fname) as f:
+            assert f.read() in expect_begin
+
+
+def test_bbb_repo_find_single_file_per_timestamp(tmpdir):
+    repo = bbb.Repository(str(tmpdir), directory_depths=[3]*3 + [3])
+    span = 500
+    begin_end_cam_ids = [(ts, ts + span, [0, 1])
+                         for ts in range(0, 100000, span)]
+
+    fill_repository(repo, begin_end_cam_ids)
+
+    find_and_assert_begin(repo, 0, expect_begin=0)
+    find_and_assert_begin(repo, 50, expect_begin=0)
+    find_and_assert_begin(repo, 500, expect_begin=500)
+    find_and_assert_begin(repo, 650, expect_begin=500)
+    find_and_assert_begin(repo, 999, expect_begin=500)
+    find_and_assert_begin(repo, 1000, expect_begin=1000)
+
+    with pytest.raises(AssertionError):
+        find_and_assert_begin(repo, 50000, 50)
+
+
+def test_bbb_repo_find_multiple_file_per_timestamp(tmpdir):
+    repo = bbb.Repository(str(tmpdir), directory_depths=[3]*3 + [3])
+    span = 500
+    begin = 1000
+    end = 100000
+    begin_end_cam_ids = [(ts, ts + span, [0, 1])
+                         for ts in range(begin, end, span)]
+    begin_end_cam_ids += [(ts, ts + span, [1, 2])
+                          for ts in range(begin, end, span)]
+
+    fill_repository(repo, begin_end_cam_ids)
+
+    find_and_assert_begin(repo, 0, expect_begin=0, nb_files_found=0)
+    find_and_assert_begin(repo, 1050, expect_begin=1000, nb_files_found=2)
+    find_and_assert_begin(repo, 1499, expect_begin=1000, nb_files_found=2)
+    find_and_assert_begin(repo, 1500, expect_begin=1500, nb_files_found=2)
+
+
+def test_bbb_create_symlinks(tmpdir):
+    repo = bbb.Repository(str(tmpdir), directory_depths=[3]*3 + [3])
+    file_params = (0, 2000, [0, 1], 'bbb')
+    symlinks = repo._create_file_and_symlinks(*file_params)
+    fname = repo.get_file_name(*file_params)
+    with open(fname, 'w') as f:
+        f.write("hello world!")
+    assert len(symlinks) == 2
+    assert os.path.exists(symlinks[0])
+    for symlink in symlinks:
+        with open(symlink) as f:
+            assert f.read() == "hello world!"
+
+    file_params = (1045, 4567, [0, 1], 'bbb')
+    symlinks = repo._create_file_and_symlinks(*file_params)
+    assert len(symlinks) == 3
+
+    file_params = (1045, 1999, [0, 1], 'bbb')
+    symlinks = repo._create_file_and_symlinks(*file_params)
+    assert len(symlinks) == 0
+
