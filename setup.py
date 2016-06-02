f0c034456d54d06426279011c3d78e5b083104e8
berleon <github@leon-sixt.de>
2016-06-02 20:37:46 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+dep_links = [str(req_line.url) for req_line in install_reqs]
 
 
-setup(name='bb_binary',
-      version='0.1',
-      description='BeesBook Binary',
-      author='Leon Sixt',
-      author_email='mail@leon-sixt.de',
-      url='https://github.com/BioroboticsLab/bb_binary/',
-      packages=['bb_binary'],
-      package_dir={'': 'python'},
-      package_data={'bb_binary': ['*.capnp']}
+setup(
+    name='bb_binary',
+    version='0.1',
+    description='BeesBook Binary',
+    author='Leon Sixt',
+    author_email='mail@leon-sixt.de',
+    url='https://github.com/BioroboticsLab/bb_binary/',
+    install_requires=reqs,
+    dependency_links=dep_links,
+    packages=['bb_binary'],
+    package_dir={'': 'python'},
+    package_data={'bb_binary': ['*.capnp']}
 )
