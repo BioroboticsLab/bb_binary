029babb820577e364d01a554aaf7db77aecb7b05
berleon <github@leon-sixt.de>
2016-06-04 20:40:12 +0200

    Fix fill_repository

--- python/test/conftest.py
+++ python/test/conftest.py
@@ -4,3 +4,6 @@ def fill_repository(repo, begin_end_cam_id):
     for begin, end, cam_id in begin_end_cam_id:
         params = begin, end, cam_id, 'bbb'
         repo._create_file_and_symlinks(*params)
+        fname = repo._get_filename(*params)
+        with open(fname, 'w') as f:
+            f.write(str(begin))
