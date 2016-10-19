951bb7a4127552197a29c590773e76b501a96d58
Benjamin Rosemann <benjamin.rosemann@fu-berlin.de>
2016-10-19 09:48:56 +0200

    Add some constants to module level [skip ci].

--- python/bb_binary/__init__.py
+++ python/bb_binary/__init__.py
@@ -1,28 +1,36 @@
 # -*- coding: utf-8 -*-
-import bb_binary.converting as c
-import bb_binary.repository as r
-import bb_binary.parsing as p
+import bb_binary.constants as constants
+import bb_binary.converting as converting
+import bb_binary.repository as repository
+import bb_binary.parsing as parsing
 
 # TODO: Add warning about windows symlinks
 
+Frame = constants.Frame
+FrameContainer = constants.FrameContainer
+DataSource = constants.DataSource
+DetectionCVP = constants.DetectionCVP
+DetectionDP = constants.DetectionDP
+DetectionTruth = constants.DetectionTruth
 
-convert_frame_to_numpy = c.convert_frame_to_numpy
-build_frame_container = c.build_frame_container
-build_frame_container_from_df = c.build_frame_container_from_df
 
-load_frame_container = r.load_frame_container
-Repository = r.Repository
+convert_frame_to_numpy = converting.convert_frame_to_numpy
+build_frame_container = converting.build_frame_container
+build_frame_container_from_df = converting.build_frame_container_from_df
 
-dt_to_str = p.dt_to_str
-get_fname = p.get_fname
-get_video_fname = p.get_video_fname
-get_timezone = p.get_timezone
-int_id_to_binary = p.int_id_to_binary
-parse_cam_id = p.parse_cam_id
-parse_fname = p.parse_fname
-parse_image_fname = p.parse_image_fname
-parse_image_fname_iso = p.parse_image_fname
-parse_image_fname_beesbook = p.parse_image_fname_beesbook
-parse_video_fname = p.parse_video_fname
-to_datetime = p.to_datetime
-to_timestamp = p.to_datetime
+load_frame_container = repository.load_frame_container
+Repository = repository.Repository
+
+dt_to_str = parsing.dt_to_str
+get_fname = parsing.get_fname
+get_video_fname = parsing.get_video_fname
+get_timezone = parsing.get_timezone
+int_id_to_binary = parsing.int_id_to_binary
+parse_cam_id = parsing.parse_cam_id
+parse_fname = parsing.parse_fname
+parse_image_fname = parsing.parse_image_fname
+parse_image_fname_iso = parsing.parse_image_fname
+parse_image_fname_beesbook = parsing.parse_image_fname_beesbook
+parse_video_fname = parsing.parse_video_fname
+to_datetime = parsing.to_datetime
+to_timestamp = parsing.to_datetime
