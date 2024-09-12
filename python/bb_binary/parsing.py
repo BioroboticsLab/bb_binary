f2d0946a8ddc9aad03d4ac20dd11f137649fc060
jacobdavidson <jddavid@umich.edu>
2024-09-12 10:27:29 +0200

    fixed basler filename fractional seconds parsing and restructured file for easier editing

--- python/bb_binary/parsing.py
+++ python/bb_binary/parsing.py
@@ -14,9 +14,12 @@ import pytz
 import re
 
 _TIMEZONE = pytz.timezone('Europe/Berlin')
+def get_timezone():
+    return _TIMEZONE
 
-
+## Generating names from timestamps
 def dt_to_str(dt):
+    """Converts a datetime object to a formatted string."""
     dt = to_datetime(dt)
     isoformat = "%Y-%m-%dT%H_%M_%S"
 
@@ -28,20 +31,34 @@ def dt_to_str(dt):
     else:
         raise Exception("Got a datetime object not in UTC. Allways use UTC.")
 
-
 def get_fname(camIdx, dt):
+    """Generates a filename based on the camera ID and datetime."""
     dt = to_datetime(dt)
     return ("Cam_{cam}_{ts}").format(cam=camIdx, ts=dt_to_str(dt))
 
-
 def get_video_fname(camIdx, begin, end):
+    """Generates a video filename based on the camera ID, begin and end timestamps."""
     return get_fname(camIdx, begin) + "--" + dt_to_str(end)
 
+## Datetime to/from Unix timestamps
+def to_datetime(t):
+    if type(t) in (int, float):
+        dt = datetime.fromtimestamp(t, tz=pytz.utc)
+        return dt
+    elif type(t) == datetime:
+        return t
+    else:
+        raise TypeError("Cannot convert {} to datetime".format(t))
 
-def get_timezone():
-    return _TIMEZONE
+def to_timestamp(dt):
+    try:
+        return dt.timestamp()
+    except AttributeError:  # python 2
+        utc_naive = dt.replace(tzinfo=None) - dt.utcoffset()
+        timestamp = (utc_naive - datetime(1970, 1, 1)).total_seconds()
+        return timestamp
 
-
+## Helper functions for ID's
 def int_id_to_binary(int_id, nb_bits=12):
     """Helper to convert an id represented as integer to a bit array.
 
@@ -67,7 +84,6 @@ def int_id_to_binary(int_id, nb_bits=12):
         int_id >>= 1
         pos -= 1
     return result
-
 
 def binary_id_to_int(decoded_id, threshold=0.5, endian='big'):
     """Helper to convert an id represented as bit array to an integer.
@@ -96,23 +112,30 @@ def binary_id_to_int(decoded_id, threshold=0.5, endian='big'):
     return int(bitarray([bit >= threshold for bit in decoded_id], endian=endian).to01(), 2)
 
 
-def parse_cam_id(fname):
-    fname = os.path.basename(fname)
-    camIdx = fname.split("_")[1]
-    return int(camIdx)
-
-
-def parse_fname(fname):
-    fname = os.path.basename(fname)
-    try:
-        return parse_video_fname(fname, 'iso')
-    except:
-        try:
-            return parse_video_fname(fname, 'beesbook')
-        except:
-            camIdx, dtime = parse_image_fname(fname)
-            return camIdx, dtime, dtime
-
+## image - frame parsing functions, with basler helper function.
+# Frames have a single camera id and a single timestamp,  e.g. cam-0_20240621T100554.249052.472Z
+def parse_image_fname(fname, format='auto'):
+    """Parses a filename to extract the camera ID and timestamp."""
+    parsers = {
+        'beesbook': parse_image_fname_beesbook,
+        'iso': parse_image_fname_iso,
+        'basler': parse_image_fname_basler
+    }
+    basename = os.path.basename(fname)
+    if format == 'auto':
+        if basename.startswith('cam-'):  # Detect the 'basler' format: it starts with 'cam-'
+            return parse_image_fname_basler(fname)
+        elif basename.count('_') >= 3: # If the filename contains 3 or more underscores, assume 'beesbook' format
+            return parse_image_fname_beesbook(fname)
+        else:  # Try ISO format as a fallback
+            try:
+                return parse_image_fname_iso(fname)
+            except ValueError:
+                raise ValueError(f"Filename '{fname}' does not match any known format (basler, iso, beesbook).")
+    else:  # use the format passed in
+        if format not in parsers:
+            raise ValueError(f"Unknown format '{format}' provided.")
+        return parsers[format](fname)
 
 def parse_image_fname_beesbook(fname):
     basename = os.path.basename(fname)
@@ -134,16 +157,13 @@ def parse_image_fname_beesbook(fname):
     dt = get_timezone().localize(dt)
     return camIdx, dt.astimezone(pytz.utc)
 
-
 def parse_image_fname_iso(fname):
     basename = os.path.basename(fname)
     name_splitted = basename.split('.')
-
     def parse_name(name):
         _, camIdxStr, iso_str = name.split('_')
         dt = iso8601.parse_date(iso_str)
         return int(camIdxStr), dt
-
     try:
         name = '.'.join(name_splitted[:2])
         camIdx, dt = parse_name(name)
@@ -155,19 +175,15 @@ def parse_image_fname_iso(fname):
 
 def parse_image_fname_basler(fname):
     basename = os.path.basename(fname)
-    name_splitted = basename.split('.')
-
     def parse_name(name):
         camIdxStr, date_str = name.split('_')
         dt = iso8601.parse_date(basler_to_iso_format(date_str))
         return int(camIdxStr.replace('cam-', '')), dt
-
     try:
-        name = '.'.join(name_splitted[:2])
-        camIdx, dt = parse_name(name)
-    except iso8601.ParseError:
-        name = name_splitted[0]
-        camIdx, dt = parse_name(name)
+        camIdx, dt = parse_name(basename)
+    except iso8601.ParseError:  # don't save the seconds, just the minute, if there is a problem
+        name_splitted = basename.split('.')
+        camIdx, dt = parse_name(name_splitted[0])
 
     return camIdx, dt
 
@@ -181,28 +197,10 @@ def basler_to_iso_format(date_str):
     date_str = re.sub(r'Z+', 'Z', date_str)
     return date_str
 
-def parse_image_fname(fname, format='auto'):
-    if format == 'beesbook':
-        return parse_image_fname_beesbook(fname)
-    elif format == 'iso':
-        return parse_image_fname_iso(fname)
-    elif format == 'basler':
-        return  parse_image_fname_basler(fname)
-    elif format == 'auto':
-        basename = os.path.basename(fname)
-        # Detect the 'basler' format: it starts with 'cam-'
-        if basename.startswith('cam-'):
-            return parse_image_fname_basler(fname)
-        # If the filename contains 3 or more underscores, assume 'beesbook' format
-        elif basename.count('_') >= 3:
-            return parse_image_fname_beesbook(fname)
-        # Try ISO format as a fallback
-        else:
-            try:
-                return parse_image_fname_iso(fname)
-            except ValueError:
-                raise ValueError(f"Filename '{fname}' does not match any known format (basler, iso, beesbook).")
 
+
+## Video filename parsing
+# Videos have a single camera id and two timestamps for start and end,  e.g. cam-0_20240621T100553.581867.682Z--20240621T100653.461292.962Z.txt
 def parse_video_fname(fname, format='auto'):
     def beesbook_parse():
         begin_name, end_name = basename.split('_TO_')
@@ -229,52 +227,44 @@ def parse_video_fname(fname, format='auto'):
         name, _ = os.path.splitext(basename)
         camIdx, isotimespan = name.split('_')
         start, end = isotimespan.split('--')
-        
         # Correct the date format
         start = basler_to_iso_format(start)
         end = basler_to_iso_format(end)
-        
         return int(camIdx.replace('cam-', '')), iso8601.parse_date(start), iso8601.parse_date(end)
 
-
     basename = os.path.basename(fname)
-    if format == 'beesbook':
-        return beesbook_parse()
-    elif format == 'bbb':
-        return bbb_parse()
-    elif format == 'iso':
-        return iso_parse()
-    elif format == 'basler':
-        return basler_parse()
-    elif format == 'auto':
+    parsers = {
+        'beesbook': beesbook_parse,
+        'iso': iso_parse,
+        'bbb': bbb_parse,
+        'basler': basler_parse
+    }    
+    # If a specific format is provided, use the corresponding parser
+    if format in parsers:
+        return parsers[format]()
+    # If format is 'auto', try each parser in order
+    for parser in parsers.values():
         try:
-            return beesbook_parse()
-        except ValueError:
-            try:
-                return bbb_parse()
-            except ValueError:
-                try:
-                    return iso_parse()
-                except ValueError:
-                    return basler_parse()
-    else:
-        raise ValueError("Unknown format {}.".format(format))
+            return parser()
+        except (ValueError, KeyError):
+            continue
+    # If no parser succeeded, raise an error
+    raise ValueError(f"Filename '{fname}' does not match any known format (beesbook, iso, bbb, basler).")
 
 
-def to_datetime(t):
-    if type(t) in (int, float):
-        dt = datetime.fromtimestamp(t, tz=pytz.utc)
-        return dt
-    elif type(t) == datetime:
-        return t
-    else:
-        raise TypeError("Cannot convert {} to datetime".format(t))
+## General parsing functions
+def parse_cam_id(fname):
+    fname = os.path.basename(fname)
+    camIdx = fname.split("_")[1]
+    return int(camIdx)
 
-
-def to_timestamp(dt):
+def parse_fname(fname):
+    fname = os.path.basename(fname)
     try:
-        return dt.timestamp()
-    except AttributeError:  # python 2
-        utc_naive = dt.replace(tzinfo=None) - dt.utcoffset()
-        timestamp = (utc_naive - datetime(1970, 1, 1)).total_seconds()
-        return timestamp
+        return parse_video_fname(fname, 'iso')
+    except:
+        try:
+            return parse_video_fname(fname, 'beesbook')
+        except:
+            camIdx, dtime = parse_image_fname(fname)
+            return camIdx, dtime, dtime
